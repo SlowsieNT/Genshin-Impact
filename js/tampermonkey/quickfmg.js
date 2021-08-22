5ccb2f7cb164cd34e5a6720e2efc493d9f7831dc
SlowsieNT <86121252+SlowsieNT@users.noreply.github.com>
2021-08-22 14:58:02 +0000

    fancy comments for people to read

--- js/tampermonkey/quickfmg.js
+++ js/tampermonkey/quickfmg.js
@@ -14,10 +14,14 @@
 (function() {
     'use strict';
 	function GenerateString(aLength) {
+		// couldn't be bothered to use one Random() call, yes
 		for (var i = 0, r = ""; i < aLength; i++) {
+			// we use bitwise flex for flooring number in range: 2147483647, -2147483648
 			var t = [48,97][Math.random()*2>>0];
+			// legend says ternary operator is slower than "if", couldn't be bothered again
 			r += t>48?String.fromCharCode(t+Math.random()*6>>0):Math.random()*10>>0;
 		}
+		// people define variables outside "for", oh no
 		return r;
 	}
 	function GenerateMD(x) {
@@ -30,17 +34,20 @@
 	// once user click on logo, it will redirect to random email link
 	$(".navbar-brand").click(function () {
 		// generate random things
+		// we try to randomize all we can randomize
 		var vDomain = GenerateMD(),
 			vLengthRng = 15+Math.random()*16>>0,
 			vFancyLetter = String.fromCharCode(97+6+Math.random()*16>>0),
 			vUsername = GenerateString(vLengthRng)+vFancyLetter+Date.now();
 		// copy generated mail to clipboard
+		// it could be done other way, couldn't be bothered again (#3)
 		GM_setClipboard(vUsername+vDomain);
 		// navigate to new mail
 		location.href = 'http://www.fakemailgenerator.com/inbox/' +
 		vDomain.slice(1) + '/' + vUsername + '/';
 	}).css("background-color", "green").attr("href", "#");
 	// check if mail received every 255ms
+	// and then after copying stop the script
 	var vTmrChk1 = setInterval(function () {
 		var vParagraph = $("p:contains(verifi)");
 		if (vParagraph.length)
